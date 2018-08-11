6db32c27c068f010926e51ed89a9bf41ca0ddd4a
Will <doggy.huang@gmail.com>
2018-08-12 01:18:12 +0800

    完整的 FormsAuthentication 實作 (包含自訂角色實作)
    簡介 ASP.NET 表單驗證 (FormsAuthentication) 的運作方式
    https://blog.miniasp.com/post/2008/02/20/Explain-Forms-Authentication-in-ASPNET-20.aspx
    
    如何在 ASP.NET Web API 2 專案中啟用表單驗證 (Forms Authentication)
    https://blog.miniasp.com/post/2016/09/22/Enable-FormsAuthentication-in-ASPNET-Web-API-2.aspx
    
    ASP.NET 自訂角色的方式（不用實做 Role Provider）
    https://blog.miniasp.com/post/2008/06/11/How-to-define-Roles-but-not-implementing-Role-Provider-in-ASPNET.aspx

--- WebApi2/Controllers/AccountsController.cs
+++ WebApi2/Controllers/AccountsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
 using System.Web.Http;
 using System.Web.Security;
 using WebApi2.Models;
@@ -13,16 +15,38 @@ namespace WebApi2.Controllers
     public class AccountsController : ApiController
     {
         [HttpPost, Route("login")]
-        public IHttpActionResult Login(LoginViewModel user)
+        public HttpResponseMessage Login(LoginViewModel user)
         {
             if (user.Username == "will" && user.Password == "123")
             {
-                // 這段程式碼預設會寫入 Location 標頭，所以最好要回應 HTTP 302
-                FormsAuthentication.RedirectFromLoginPage(user.Username, false);
-                return RedirectToRoute(nameof(GetProfile), null);
+                string role = "Admin";
+                bool isPersistent = false;
+
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                    user.Username,
+                    DateTime.Now,
+                    DateTime.Now.AddMinutes(30),
+                    isPersistent,
+                    role,
+                    FormsAuthentication.FormsCookiePath);
+
+                // Encrypt the ticket.
+                string encTicket = FormsAuthentication.Encrypt(ticket);
+
+                // Create the cookie.
+                var resp = new HttpResponseMessage();
+                resp.StatusCode = HttpStatusCode.NoContent;
+
+                var cookie = new CookieHeaderValue(FormsAuthentication.FormsCookieName, encTicket);
+                cookie.Expires = DateTimeOffset.Now.AddDays(1);
+                cookie.Domain = Request.RequestUri.Host;
+                cookie.Path = "/";
+                resp.Headers.AddCookies(new CookieHeaderValue[] { cookie });
+
+                return resp;
             }
 
-            return BadRequest("帳號或密碼錯誤");
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "帳號或密碼錯誤");
         }
 
         [HttpGet, Route("logout")]
@@ -37,7 +61,8 @@ namespace WebApi2.Controllers
             return Ok(new
             {
                 IsAuthenticated = User.Identity.IsAuthenticated,
-                Username = User.Identity.Name
+                Username = User.Identity.Name,
+                IsAdminRole = User.IsInRole("Admin")
             });
         }
     }
